733b523bed1f94f3aec3102d9118cc3f361aa2df
AdhiraRayh <Adhirarayhan689@gmail.com>
2025-10-22 17:08:12 +0700

    edede

---
+++ wleeee/src/alamak.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class alamak {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        int sum = x + y;
+        System.out.println("Sum = "+ sum);
+
+    }
+}
